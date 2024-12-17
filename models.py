5223ff099d8d343c6700c9f64a864703a6f0c262
inal <balkarov05@gmail.com>
2024-12-17 17:25:41 +0300

    created validator and field type models

--- models.py
+++ models.py
@@ -1,19 +1,38 @@
 from pydantic import (
     BaseModel,
-    StringConstraints,
+    field_validator,
 )
 
-from typing import Dict, Annotated, List
+from typing import Dict
+from enum import Enum
+
+import utils
 
 
-class FieldModel(BaseModel):
-    name: Annotated[str, StringConstraints(min_length=1)]
+class FieldType(Enum):
+    email = "email"
+    phone = "phone"
+    date = "date"
+    text = "text"
 
 
 class FormTemplate(BaseModel):
     name: str
-    fields: List[FieldModel]
+    fields: Dict[str, str]
 
 
 class FormData(BaseModel):
-    data: Dict[str, str]
+    data: Dict[str, str] = None
+
+    @field_validator("data", mode="before")
+    def define_field_type(cls, values):
+        for key, value in values.items():
+            if utils.is_date(value):
+                values[key] = FieldType.date.value
+            elif utils.is_phone_number(value):
+                values[key] = FieldType.phone.value
+            elif utils.is_email(value):
+                values[key] = FieldType.email.value
+            else:
+                values[key] = FieldType.text.value
+        return values
